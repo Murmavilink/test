aabb5ed1d358f8a87b19907ca03c40085d037e50
Mur <murmavilink@gmail.com>
2023-01-11 10:11:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const form = document.getElementById('form');
+const name = document.getElementById('text');
+const pass = document.getElementById('number');
+
+const sendData = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-type': 'multipart/form-data',
+        },
+    }).then(res => res.json());
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+sendData('https://jsonplaceholder.typicode.com/posts', data)
+    .then(data => {
+        console.log(data);
+    });
+
+});
+
+
+
+
+
+
+
+
+// const user = fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         title: 'foo',
+//         body: 'bar',
+//         userId: 1,
+//     }),
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//     },
+// });
+
+// user.then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(error => {
+//         console.log(error);
+//     });
