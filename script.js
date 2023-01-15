e85bb3246d510477d6e00d13c744aa55d1e3f057
Mur <murmavilink@gmail.com>
2023-01-15 16:03:42 +0600

    done (currency)

--- script.js
+++ script.js
@@ -1,45 +1,110 @@
 'use strict';
 
-const list = document.getElementById('list');
-const selectCars = document.getElementById('cars');
 
-let carsObj = {};
+const myHeaders = new Headers();
+myHeaders.append("apikey", "CZH8qJealtsViSl2N9NicLvlEKNRirA7");
+const inputAmount = document.getElementById('input');
+const form = document.getElementById('form');
+const spinner = document.getElementById('spinner');
 
-
-const showCar = (car) => {
-    const elementOne = document.createElement('li');
-    const elementTwo = document.createElement('li');
-
-    elementOne.textContent = `Тачка ${car.brand} ${car.model}`;
-    elementTwo.textContent = `Цена: ${car.price}`;
-    list.append(elementOne);
-    list.append(elementTwo);
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
 };
 
-selectCars.addEventListener('change', () => {
-    const option = selectCars.options[selectCars.selectedIndex];
-    list.innerHTML = '';
+const getResult = () => {
+    const amount = inputAmount.value;
+    const fromValue = document.getElementById('from').value;
+    const toValue = document.getElementById('to').value;
+    const resultInput = document.getElementById('result');
+    if (!amount) {
+        spinner.style.display = "none";
+        return;
+    }
 
-    if(option.value === 'bmw') {
-        // let id = selectCars.selectedIndex - 1;
-        showCar(carsObj[selectCars.selectedIndex - 1]);
-    } else if(option.value === 'volvo') {
-        showCar(carsObj[selectCars.selectedIndex - 1]);
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${toValue}&from=${fromValue}&amount=${amount}`,
+        requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            spinner.style.display = "none";
+            resultInput.value = (result.result).toFixed(2);
+        })
+        .catch(error => console.log('error', error));
+};
+
+inputAmount.addEventListener('input', (e) => {
+    e.target.value = e.target.value.replace(/\D+/g, '');
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    try {
+        spinner.style.display = "inline-block";
+        getResult();
+    } catch (error) {
+        console.log(error.message);
     }
 
 });
 
 
-const getCars = (url) => {
-    return fetch(url).then(res => res.json());
-};
 
 
-getCars('cars.json').then(data => {
-    carsObj = data.cars;
-}).catch(error => {
-    console.dir(error.message);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const list = document.getElementById('list');
+// const selectCars = document.getElementById('cars');
+
+// let carsObj = {};
+
+
+// const showCar = (car) => {
+//     const elementOne = document.createElement('li');
+//     const elementTwo = document.createElement('li');
+
+//     elementOne.textContent = `Тачка ${car.brand} ${car.model}`;
+//     elementTwo.textContent = `Цена: ${car.price}`;
+//     list.append(elementOne);
+//     list.append(elementTwo);
+// };
+
+// selectCars.addEventListener('change', () => {
+//     const option = selectCars.options[selectCars.selectedIndex];
+//     list.innerHTML = '';
+
+//     if(option.value === 'bmw') {
+//         // let id = selectCars.selectedIndex - 1;
+//         showCar(carsObj[selectCars.selectedIndex - 1]);
+//     } else if(option.value === 'volvo') {
+//         showCar(carsObj[selectCars.selectedIndex - 1]);
+//     }
+
+// });
+
+
+// const getCars = (url) => {
+//     return fetch(url).then(res => res.json());
+// };
+
+
+// getCars('cars.json').then(data => {
+//     carsObj = data.cars;
+// }).catch(error => {
+//     console.dir(error.message);
+// });
 
     
 
